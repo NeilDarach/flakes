9005d461472d15ca66158e7b56f7040d3b586921
Neil Darach <neil.darach@gmail.com>
2025-10-30 16:55:31 +0100

    fix

--- rust/flake.nix
+++ rust/flake.nix
@@ -20,7 +20,7 @@
         };
       };
       config = {
-        overlays = if true then
+        overlays.default = if true then
           final: prev: {
             rustToolChain = let rust = prev.rust-bin;
             in if builtins.pathExists ./rust-toolchain.toml then
