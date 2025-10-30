6acb7a380f19acae8594629d84db5f97a10eec29
Neil Darach <neil.darach@gmail.com>
2025-10-30 22:11:31 +0100

    Functions

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,38 +1,39 @@
 {
-  description = "Environment for developing rust programs";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils = { url = "github:numtide/flake-utils"; };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs = { nixpkgs.follows = "nixpkgs"; };
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
+
   };
 
-  outputs = inputs@{ nixpkgs, rust-overlay, ... }: {
+  outputs = { nixpkgs, rust-overlay, ... }: {
+    overlays.default = [
+      (import rust-overlay)
+      (prev: final: {
+        rustToolChain = let rust = prev.rust-bin;
+        in if builtins.pathExists ./rust-toolchain.toml then
+          rust.fromRustupToolchainFile ./rust-toolchain.toml
+        else if builtins.pathExists ./rust-toolchain then
+          rust.fromRustupToolchainFile ./rust-toolchain
+        else
+          rust.stable.latest.default.override {
+            extensions = [ "rust-src" "rustfmt" ];
+          };
 
-    nixosModules.default = { config, inputs, pkgs, lib, ... }: {
-      options = {
-        rustOptions.enabled = lib.mkEnableOption "If rust should be loaded";
-        rustOptions.extensions = lib.mkOption {
-          type = lib.types.listOf lib.types.str;
-          default = [ ];
-          description = "Which extensions to include";
+      })
+    ];
+    makeDevShell = (pkgs: extra:
+      pkgs.mkShell {
+        packages = with pkgs; [ rustToolChain just ] ++ (extra.packages or [ ]);
+        env = {
+          RUST_SRC_PATH = "${pkgs.rustToolChain}/lib/rustlib/src/rust/library";
         };
-      };
-      overlays.default = [
-        rust-overlay.overlays.default
-        (final: prev: {
-          rustToolChain = let rust = prev.rust-bin;
-          in if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = config.rustOptions.extensions;
-            };
-        })
-      ];
-    };
+      });
   };
 }
