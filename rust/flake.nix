05b663f04bce0bc30ee2136a26edc188bd65f6ef
Neil Darach <neil.darach@gmail.com>
2025-10-30 16:15:31 +0100

    Moved to rust overlay into a flake

---
+++ rust/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Environment for developing rust programs";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, rust-overlay, ... }: {
+
+    nixosModules.default = { config, inputs, pkgs, lib, ... }: {
+      options = {
+        rustOptions.enabled = lib.mkEnableOption "If rust should be loaded";
+        rustOptions.extensions = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [ ];
+          description = "Which extensions to include";
+        };
+      };
+      config = {
+        overlays.default = if config.rustOptionsEnabled then
+          final: prev: {
+            rustToolChain = let rust = prev.rust-bin;
+            in if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.stable.latest.default.override {
+                extensions = config.rustOptions.extensions;
+              };
+          }
+        else
+          final: prev: { };
+      };
+    };
+  };
+}
